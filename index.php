7cd13302b1dfc1b3e3f058ae852c5bd572d4e091
Sohan817 <hsohan509@gmail.com>
2023-09-21 01:47:09 +0600

    Variable and Data types in php

--- index.php
+++ index.php
@@ -6,11 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-         echo "Hello World";
-    ?>
-    <?php if(true){ ?>
-        <p>Hello PHP</p>
-    <?php } ?>
+  <?php
+  //Variable and naming convention
+  $_name = "Sohan";
+  echo $_name;
+  ?>
+  <!-- Data Type -->
+  <?php
+  $string = "Sohan";
+  $int = 10;
+  $float = 20.45;
+  $bool = true;
+  echo $float;
+  //Array Type
+  $names = array("Sohan","Nadim","Suborna");
+  $number = [1,2,4,6,7];
+  print_r($number[0]);
+  print_r($names);
+
+  //object 
+  //$object = new Car();
+
+  //Embedding HTML and php
+  $name = "Shohan Hossain";
+  $test = $name;
+  ?>
+  <p>Hi my name is <?php echo $name?>. I am learning php</p>
 </body>
 </html>
