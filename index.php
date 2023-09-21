aa288328be9f5767710006bcf1c75c6d974aad8a
Sohan817 <hsohan509@gmail.com>
2023-09-21 15:10:18 +0600

    Built in superglobal variable in PHP

--- index.php
+++ index.php
@@ -1,36 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  <?php
-  //Variable and naming convention
-  $_name = "Sohan";
-  echo $_name;
-  ?>
-  <!-- Data Type -->
-  <?php
-  $string = "Sohan";
-  $int = 10;
-  $float = 20.45;
-  $bool = true;
-  echo $float;
-  //Array Type
-  $names = array("Sohan","Nadim","Suborna");
-  $number = [1,2,4,6,7];
-  print_r($number[0]);
-  print_r($names);
+ <!DOCTYPE html>
+ <html lang="en">
 
-  //object 
-  //$object = new Car();
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+ </head>
 
-  //Embedding HTML and php
-  $name = "Shohan Hossain";
-  $test = $name;
-  ?>
-  <p>Hi my name is <?php echo $name?>. I am learning php</p>
-</body>
-</html>
+ <body>
+   <!-- SERVER super global variable -->
+   <?php
+    echo $_SERVER['DOCUMENT_ROOT'];
+    echo "<br>";
+    echo $_SERVER['PHP_SELF'];
+    echo "<br>";
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>";
+    echo $_SERVER['REQUEST_METHOD'];
+    ?>
+   <!-- GET and POSST suuper global variable -->
+   <?php
+    echo $_GET['name'];
+    echo $_GET['eyecolor'];
+    echo "<br>";
+    echo $_REQUEST['name'];
+    echo "<br>";
+    echo $_COOKIE['name'];
+    echo "<br>";
+    $_SESSION['username'] = "Sohan";
+    echo $_SESSION['username'];
+    ?>
+ </body>
+
+ </html>
