c6282d94d99a0a06d560447ccf3eaf84cd2b5d25
Sohan817 <hsohan509@gmail.com>
2023-09-23 00:13:52 +0600

    Created a calculator

--- index.php
+++ index.php
@@ -4,43 +4,64 @@
  <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+ </head>
+ <title>Calculator</title>
  </head>
 
  <body>
+   <div class="container text-center" style="width: 12rem;">
+     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+       <input type="number" name="num1" class="form-control" placeholder="Enter first Number" required><br><br>
+       <select name="operator">
+         <option value="addition">+</option>
+         <option value="substraction">-</option>
+         <option value="multiply">*</option>
+         <option value="divition">/</option>
+       </select><br><br>
+       <input type="number" name="num2" class="form-control" placeholder="Enter second Number" required><br><br>
+       <button class="btn btn-primary">Calculate</button>
+     </form>
+   </div>
    <?php
-    //If else statement
-    $a = 1;
-    $b = 2;
-    $bool = true;
-
-    if ($a < $b && !$bool) {
-      echo "First statement is true";
-    } else if ($a < $b && $bool) {
-      echo "Second statement is true";
-    } else {
-      echo "Third statement is true";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      //Grab data from inputs
+      $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT);
+      $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT);
+      $operator = htmlspecialchars($_POST["operator"]);
+      //Error Handlers
+      $error = false;
+      if (empty($num1) || empty($num1) || empty($operator)) {
+        echo "<br>";
+        echo "<p class='text-center'>Fill in all fields</p>";
+        $error = true;
+      }
+      if (is_numeric($num1) || is_numeric($num2)) {
+        echo "<br>";
+        echo "<p class='text-center'>Only write number</p>";
+        $error = true;
+      }
+      //Calculate the number if no errors
+      $value = 0;
+      switch ($operator) {
+        case "addition":
+          $value = $num1 + $num2;
+          break;
+        case "substraction":
+          $value = $num1 - $num2;
+          break;
+        case "multiply":
+          $value = $num1 * $num2;
+          break;
+        case "divition":
+          $value = $num1 / $num2;
+          break;
+        default:
+          echo "<p class='text-center'>Something went wrong</p>";
+          break;
+      }
     }
-    echo "<br>";
-    //Switch 
-    switch ($a) {
-      case 1:
-        echo "First statement is true";
-        break;
-      case 2:
-        echo "Second statement is true";
-        break;
-      default:
-        echo "Third statement is true";
-    }
-    echo "<br>";
-    //Match statement
-    $result = match ($a) {
-      1, 3, 5 => "Var a is one",
-      2 => "Var a is two",
-      default => "None were to return"
-    };
-    echo $result
+    echo "<p class='text-center'>Result of " . $operator . " = " . $value . "</p>";
     ?>
  </body>
 
