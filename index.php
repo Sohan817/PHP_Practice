dea4fe1d0afbfe60a2b5c9ee1b5eb01df3723dc8
Sohan817 <hsohan509@gmail.com>
2023-09-24 02:28:56 +0600

    Constant in PHP

--- index.php
+++ index.php
@@ -1,54 +1,12 @@
 <?php
-//Global scope var
-$test = "Sohan";
+define("PI", 3.1416);
+define("NAME", "Sohan");
+define("IS_ADMIN", true);
 
-function myFunction()
+echo PI;
+
+function myConstant()
 {
-  //local scope var
-  $localVar = "Nadim";
-  //use of local vaer inside the function
-  return $localVar;
+  echo NAME;
 }
-echo myFunction();
-
-function myFunction1()
-{
-  //global var used in local scope
-  global $test;
-  //use of local var inside the function
-  return $test;
-}
-echo myFunction1();
-
-//using global var by super global variable
-$test1 = "Suborna";
-function myFunction2()
-{
-  //use of local var inside the function
-  return $GLOBALS["test1"];
-}
-echo myFunction2();
-
-//Static scope variable
-function staticScope()
-{
-  static $staticVar = 0;
-
-  $staticVar++;
-
-  return $staticVar;
-}
-echo staticScope();
-echo staticScope();
-
-//class scope 
-class Mycllass
-{
-  //define a class var
-  public $myVar = "Hello class";
-  //define a class function
-  public function myFunc()
-  {
-    return $this->myVar;
-  }
-}
+myConstant();
