5148ce178d2a46923a34f73e3b89db9fa238e662
Sohan817 <hsohan509@gmail.com>
2023-09-24 17:11:15 +0600

    loops in php

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
-define("PI", 3.1416);
-define("NAME", "Sohan");
-define("IS_ADMIN", true);
-
-echo PI;
-
-function myConstant()
-{
-  echo NAME;
+//for loop
+for ($i = 0; $i < 10; $i++) {
+  echo "This is for loop " . $i . "<br>";
 }
-myConstant();
+
+//while lopp
+$test = 5;
+while ($test < 10) {
+  echo "This is while loop " . $test . "<br>";
+  $test++;
+}
+//do while loop
+$test = 10;
+do {
+  echo "This is do while loop " . $test . "<br>";
+  $test++;
+} while ($test < 10);
+
+//foreach loop 
+$fruites1 = ["Apple", "Mango", "Banana"];
+foreach ($fruites1 as $fruite) {
+  echo "This is a " . $fruite . "<br>";
+}
+
+//foreach loop with asssociative array
+$fruites2 = ["Apple" => "Red", "Mango" => "Green", "Banana" => "Yellow"];
+foreach ($fruites2 as $fruite => $color) {
+  echo "This is a " . $fruite . ",Whose color is " . $color . "<br>";
+}
